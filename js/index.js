5424a350e9a43ba6c96b9dec11c4754a835ad095
Guanwf <37936783@qq.com>
2026-07-11 22:13:17 +0800

    debug

--- js/index.js
+++ js/index.js
@@ -987,6 +987,7 @@ kubectl get deploy,sts -n roc-uat -o custom-columns="KIND:.kind,NAME:.metadata.n
 
             {category: "📊Monitor", text: "Prometheus", url: "http://prometheus.local", desc: "监控大盘" },
             {category: "📊Monitor", text: "Grafana", url: "http://grafana.local", desc: "图表展示"},
+            {category: "📊Monitor", text:"监控操作说明", url: "https://xd20al46gl.feishu.cn/docx/DsMfd4awdowgeLx0HNecUQYxnzb", desc: "监控操作说明"},
 
             { category: "⚽️NetWork",text:"wireguard", url: "https://www.wireguard.com/quickstart/", desc: "vpc"},
             { category: "⚽️NetWork",text:"centralops-DNS-Domain -Check-Traceroute", url: "https://centralops.net/", desc: ""},
